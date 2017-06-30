a44fb7e6c3445a0486dfff1f6a7e8d80e30f2501
Gregor <gregor@martynus.net>
2017-06-30 16:01:47 -0700

    feat: initial version

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = probotPlugin
+
+const handlePullRequestChange = require('./lib/handle-pull-request-change')
+
+function probotPlugin (robot) {
+  robot.on('pull_request.opened', handlePullRequestChange.bind(null, robot))
+  robot.on('pull_request.edited', handlePullRequestChange.bind(null, robot))
+}
