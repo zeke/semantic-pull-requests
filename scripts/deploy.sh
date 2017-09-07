61d6497dc5372f41050d446f1845d9fdeac4a319
Gregor <gregor@martynus.net>
2017-09-06 21:58:40 -0700

    chore(deploy): pass token to `now rm`

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@
 
 # https://github.com/zeit/now-cli/issues/817
 npx now@7.1.1 --public --token=$NOW_TOKEN
-npx now@7.1.1 rm --safe --yes wip-bot
+npx now@7.1.1 rm --safe --yes wip-bot --token=$NOW_TOKEN
