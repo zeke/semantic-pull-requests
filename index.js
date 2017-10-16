0bc8822e5ff1152256b99378bc62646c8fd9bd7a
Gregor Martynus <gregor@martynus.net>
2017-10-16 16:51:41 -0700

    fix: set status when commits updated
    last version had a typo lol

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const handlePullRequestChange = require('./lib/handle-pull-request-change')
 function probotPlugin (robot) {
   robot.on('pull_request.opened', handlePullRequestChange.bind(null, robot))
   robot.on('pull_request.edited', handlePullRequestChange.bind(null, robot))
-  robot.on('pull_request.synchronize', handlePullRequestChange.bind(null, robot))
+  robot.on('pull_request.synchronized', handlePullRequestChange.bind(null, robot))
 }
