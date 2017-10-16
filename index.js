b2c5d01ef09b41f93c7c33cda6595344885dbfd4
Gregor Martynus <gregor@martynus.net>
2017-10-16 16:44:32 -0700

    fix: set status when commits updated
    closes #8

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const handlePullRequestChange = require('./lib/handle-pull-request-change')
 function probotPlugin (robot) {
   robot.on('pull_request.opened', handlePullRequestChange.bind(null, robot))
   robot.on('pull_request.edited', handlePullRequestChange.bind(null, robot))
+  robot.on('pull_request.synchronize', handlePullRequestChange.bind(null, robot))
 }
