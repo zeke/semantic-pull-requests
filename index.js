dc42bf1314368dd70c143e919afa63bda7d22b18
Anshuman <anshu.av97@gmail.com>
2018-02-18 10:50:06 +0530

    refactor: remove unused robot param

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function probotPlugin (robot) {
     'pull_request.opened',
     'pull_request.edited',
     'pull_request.synchronize'
-  ], handlePullRequestChange.bind(null, robot))
+  ], handlePullRequestChange)
 }
