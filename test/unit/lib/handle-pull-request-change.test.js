77aacc8926ffae68b6839db8e5f3c086749d7b6e
David Celis <me@davidcel.is>
2018-02-26 12:48:59 -0800

    feat: support labels (#46)

--- test/unit/lib/handle-pull-request-change.test.js
+++ test/unit/lib/handle-pull-request-change.test.js
@@ -16,9 +16,32 @@ describe('handlePullRequestChange', () => {
         },
         pullRequests: {
           getCommits: jest.fn().mockReturnValue({ data: [] })
+        },
+        issues: {
+          getIssueLabels: jest.fn().mockReturnValue({ data: [] })
         }
       }
     }
+  }
+
+  const createMockCommitContext = commitMessage => {
+    const context = createMockContext('Example PR title')
+
+    context.github.pullRequests.getCommits = jest.fn().mockReturnValue({
+      data: [{ commit: { message: commitMessage } }]
+    })
+
+    return context
+  }
+
+  const createMockLabelContext = labelName => {
+    const context = createMockContext('Example PR title')
+
+    context.github.issues.getIssueLabels = jest.fn().mockReturnValue({
+      data: [{ name: labelName }]
+    })
+
+    return context
   }
 
   const pendingStatusObject = {
@@ -60,6 +83,48 @@ describe('handlePullRequestChange', () => {
 
   it('creates success status if PR title does NOT contain `wip`', async () => {
     const context = createMockContext('[xxx] foo bar commit message')
+    await handlePullRequestChange(context)
+
+    expect(context.repo).lastCalledWith(successStatusObject)
+  })
+
+  it('creates pending status if a commit message contains `wip`', async () => {
+    const context = createMockCommitContext('[wip] foo bar commit message')
+    await handlePullRequestChange(context)
+
+    expect(context.repo).lastCalledWith(pendingStatusObject)
+  })
+
+  it('creates pending status if a commit message contains `do not merge`', async () => {
+    const context = createMockCommitContext('my DO NOT MERGE commit message')
+    await handlePullRequestChange(context)
+
+    expect(context.repo).lastCalledWith(pendingStatusObject)
+  })
+
+  it('creates success status if a commit message does not contain `wip` or `do not merge`', async () => {
+    const context = createMockCommitContext('my commit message')
+    await handlePullRequestChange(context)
+
+    expect(context.repo).lastCalledWith(successStatusObject)
+  })
+
+  it('creates pending status if a label contains `wip`', async () => {
+    const context = createMockLabelContext('WIP')
+    await handlePullRequestChange(context)
+
+    expect(context.repo).lastCalledWith(pendingStatusObject)
+  })
+
+  it('creates pending status if a label contains `do not merge`', async () => {
+    const context = createMockLabelContext('do not merge')
+    await handlePullRequestChange(context)
+
+    expect(context.repo).lastCalledWith(pendingStatusObject)
+  })
+
+  it('creates success status if a label does not contain `wip` or `do not merge`', async () => {
+    const context = createMockLabelContext('bug')
     await handlePullRequestChange(context)
 
     expect(context.repo).lastCalledWith(successStatusObject)
