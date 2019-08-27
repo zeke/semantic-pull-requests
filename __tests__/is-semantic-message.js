dec82a8464d301ad5ecb53d362e5cf011b5d21bd
Amos Wong <amoswwj@gmail.com>
2019-08-27 11:37:06 +0800

    feat: Allow specification of custom types

--- __tests__/is-semantic-message.js
+++ __tests__/is-semantic-message.js
@@ -2,28 +2,28 @@ const isSemanticMessage = require('../lib/is-semantic-message')
 
 describe('isSemanticMessage', () => {
   test('returns true when messages are semantic', () => {
-    expect(isSemanticMessage('fix: something')).toBe(true)
+    expect(isSemanticMessage('fix: something', ['fix'])).toBe(true)
   })
 
   test('allows parenthetical scope following the type', () => {
-    expect(isSemanticMessage('fix(subsystem): something')).toBe(true)
+    expect(isSemanticMessage('fix(subsystem): something', ['fix'])).toBe(true)
   })
 
   test('returns false on bad input', () => {
-    expect(isSemanticMessage('')).toBe(false)
-    expect(isSemanticMessage(null)).toBe(false)
-    expect(isSemanticMessage('non-semantic commit message')).toBe(false)
+    expect(isSemanticMessage('', ['fix'])).toBe(false)
+    expect(isSemanticMessage(null, ['fix'])).toBe(false)
+    expect(isSemanticMessage('non-semantic commit message', ['fix'])).toBe(false)
   })
 
   test('should check message scope', () => {
-    expect(isSemanticMessage('fix(validScope): something', ['validScope'])).toBe(true)
-    expect(isSemanticMessage('fix(invalidScope): something', ['validScope'])).toBe(false)
-    expect(isSemanticMessage('fix(): something', ['validScope'])).toBe(false)
+    expect(isSemanticMessage('fix(validScope): something', ['fix'], ['validScope'])).toBe(true)
+    expect(isSemanticMessage('fix(invalidScope): something', ['fix'], ['validScope'])).toBe(false)
+    expect(isSemanticMessage('fix(): something', ['fix'], ['validScope'])).toBe(false)
   })
 
   test('allows merge commits', () => {
-    expect(isSemanticMessage('Merge branch \'master\' into patch-1', null, true)).toBe(true)
+    expect(isSemanticMessage('Merge branch \'master\' into patch-1', ['fix'], null, true)).toBe(true)
 
-    expect(isSemanticMessage('Merge refs/heads/master into angry-burritos/US-335', ['scope1'], true)).toBe(true)
+    expect(isSemanticMessage('Merge refs/heads/master into angry-burritos/US-335', ['fix'], ['scope1'], true)).toBe(true)
   })
 })
