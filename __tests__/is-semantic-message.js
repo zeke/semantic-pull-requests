de573a0146cf001be3794c84808a838c203cf69d
Amos Wong <amoswwj@gmail.com>
2019-08-30 01:18:04 +0800

    test: Undo changes made to existing test
    "allows merge commits" test is adjusted to accomondate the
    additional validType argument

--- __tests__/is-semantic-message.js
+++ __tests__/is-semantic-message.js
@@ -2,54 +2,28 @@ const isSemanticMessage = require('../lib/is-semantic-message')
 
 describe('isSemanticMessage', () => {
   test('returns true when messages are semantic', () => {
-    expect(isSemanticMessage('fix: something', ['fix'])).toBe(true)
+    expect(isSemanticMessage('fix: something')).toBe(true)
   })
 
   test('allows parenthetical scope following the type', () => {
-    expect(isSemanticMessage('fix(subsystem): something', ['fix'])).toBe(true)
+    expect(isSemanticMessage('fix(subsystem): something')).toBe(true)
   })
 
   test('returns false on bad input', () => {
-    expect(isSemanticMessage('', ['fix'])).toBe(false)
-    expect(isSemanticMessage(null, ['fix'])).toBe(false)
-    expect(isSemanticMessage('non-semantic commit message', ['fix'])).toBe(false)
+    expect(isSemanticMessage('')).toBe(false)
+    expect(isSemanticMessage(null)).toBe(false)
+    expect(isSemanticMessage('non-semantic commit message')).toBe(false)
   })
 
   test('should check message scope', () => {
-    expect(isSemanticMessage('fix(validScope): something', ['fix'], ['validScope'])).toBe(true)
-    expect(isSemanticMessage('fix(invalidScope): something', ['fix'], ['validScope'])).toBe(false)
-    expect(isSemanticMessage('fix(): something', ['fix'], ['validScope'])).toBe(false)
+    expect(isSemanticMessage('fix(validScope): something', ['validScope'])).toBe(true)
+    expect(isSemanticMessage('fix(invalidScope): something', ['validScope'])).toBe(false)
+    expect(isSemanticMessage('fix(): something', ['validScope'])).toBe(false)
   })
 
   test('allows merge commits', () => {
-    expect(isSemanticMessage('Merge branch \'master\' into patch-1', ['fix'], null, true)).toBe(true)
+    expect(isSemanticMessage('Merge branch \'master\' into patch-1', null, null, true)).toBe(true)
 
-    expect(isSemanticMessage('Merge refs/heads/master into angry-burritos/US-335', ['fix'], ['scope1'], true)).toBe(true)
-  })
-
-  test('should check for allowed types', () => {
-    const commonConventionalCommitType = [
-      'feat',
-      'fix',
-      'refactor'
-    ]
-
-    expect(isSemanticMessage('feat: Add something', commonConventionalCommitType)).toBe(true)
-    expect(isSemanticMessage('fix: Fix something', commonConventionalCommitType)).toBe(true)
-    expect(isSemanticMessage('refactor: Updated something', commonConventionalCommitType)).toBe(true)
-
-    const customConventionalCommitType = [
-      'feat',
-      'fix',
-      'refactor',
-      'alternative'
-    ]
-    expect(isSemanticMessage('alternative: Do alternative stuff', customConventionalCommitType)).toBe(true)
-    expect(isSemanticMessage('other: Do other stuff', customConventionalCommitType)).toBe(false)
-  })
-
-  test('should fail if invalid types variable is provided', () => {
-    expect(() => isSemanticMessage('fix: Do other stuff', null)).toThrowError(TypeError)
-    expect(() => isSemanticMessage('fix: Do other stuff', undefined)).toThrowError(TypeError)
+    expect(isSemanticMessage('Merge refs/heads/master into angry-burritos/US-335', ['scope1'], null, true)).toBe(true)
   })
 })
